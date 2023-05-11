fe9702383c784bf2c24d7a33c2e856755b617459
Ashwani <60231747+Ashwani8978@users.noreply.github.com>
2023-05-11 18:42:15 +0530

    My First Commit Using GIT!

---
+++ hello_world.java
@@ -0,0 +1,14 @@
+package Java_Exercise;
+
+public class Ex1_Hello_World {
+//public is a Java keyword which declares a member's access as public.
+ public static void main(String[] args){
+//public = To call JVM from Anywhere
+//static = makes it class method so that it can be called using class name without creating an object.
+//void = main method does not return value to JVM.
+//main = name of the method which is called by JVM. 
+//String[] args = the main method accept one argument of type String array. 
+    System.out.println("Hello World!");
+    //prints any argument you pass and adds a new line after it.
+ }
+}
